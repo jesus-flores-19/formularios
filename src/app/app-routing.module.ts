0f951407b0a56fa550ab77ea393ed658b373f0bc
Jesus Arath <jesus.flores.p19@gmail.com>
2021-10-14 23:38:32 -0500

    Diseñamos rutas en el documento

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,13 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { ReactivoComponent } from './componentes/reactivo/reactivo.component';
+import { TemplateComponent } from './componentes/template/template.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {path: "template", component: TemplateComponent},
+  {path: "reactivo", component: ReactivoComponent},
+  
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
