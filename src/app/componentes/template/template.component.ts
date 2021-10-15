d092aa6effb71576437456c6daadb7c8261106ae
Jesus Arath <jesus.flores.p19@gmail.com>
2021-10-14 23:57:34 -0500

    Creamos funcion onSubmit() en formulario

--- src/app/componentes/template/template.component.ts
+++ src/app/componentes/template/template.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { NgForm } from '@angular/forms';
 
 @Component({
   selector: 'app-template',
@@ -10,6 +11,10 @@ export class TemplateComponent implements OnInit {
   constructor() { }
 
   ngOnInit(): void {
+  }
+
+  onSubmit(form: NgForm){
+    console.log(form);
   }
 
 }
