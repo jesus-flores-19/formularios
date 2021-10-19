1ecf19d8e3e524de779e1586b7030fd2c45d9a27
Jesus Arath <jesus.flores.p19@gmail.com>
2021-10-18 20:34:52 -0500

    Obtenemos paises del servicio

--- src/app/service/paises.service.ts
+++ src/app/service/paises.service.ts
@@ -8,4 +8,18 @@ import { HttpClient } from "@angular/common/http"
 export class PaisesService {
 
   constructor(public http: HttpClient) { }
+
+
+  private paises= [
+    {idpais: "MEX", nombre: "México"},
+    {idpais: "COL", nombre: "Colombia"},
+    {idpais: "BRA", nombre: "Brasil"},
+    {idpais: "EU", nombre: "Estados Unidos"},
+    {idpais: "CAN", nombre: "Canada"},
+  ]
+
+
+  obtenerPaises(){
+    return this.paises;
+  }
 }
