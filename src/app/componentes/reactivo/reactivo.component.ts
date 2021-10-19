a66640f15e9278dbb1a2612906454535295cdab6
Jesus Arath <jesus.flores.p19@gmail.com>
2021-10-18 23:35:36 -0500

    Creamos el formulario del lado de ts

--- src/app/componentes/reactivo/reactivo.component.ts
+++ src/app/componentes/reactivo/reactivo.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { FormBuilder, FormGroup } from '@angular/forms';
 
 @Component({
   selector: 'app-reactivo',
@@ -7,7 +8,20 @@ import { Component, OnInit } from '@angular/core';
 })
 export class ReactivoComponent implements OnInit {
 
-  constructor() { }
+  forma: FormGroup;
+
+  constructor(private fb: FormBuilder) {
+    this.forma = this.fb.group({
+      nombre: [""],
+      apellido: [""],
+      correo: [""]
+    });
+   }
+
+   guardar(){
+     console.log(this.forma);
+     
+   }
 
   ngOnInit(): void {
   }
