b8e45b49fccf667cc5e234485c60a4911c2df617
Jesus Arath <jesus.flores.p19@gmail.com>
2021-10-18 20:32:17 -0500

    Importamos httpClient

--- src/app/service/paises.service.ts
+++ src/app/service/paises.service.ts
@@ -1,9 +1,11 @@
 import { Injectable } from '@angular/core';
+import { HttpClient } from "@angular/common/http"
+ 
 
 @Injectable({
   providedIn: 'root'
 })
 export class PaisesService {
 
-  constructor() { }
+  constructor(public http: HttpClient) { }
 }
