29309291ec7e4473635ef15167ad704d24c5ae72
Jesus Arath <jesus.flores.p19@gmail.com>
2021-10-19 22:56:05 -0500

    Validacion general del formulario

--- src/app/service/validadores.service.ts
+++ src/app/service/validadores.service.ts
@@ -1,5 +1,5 @@
 import { Injectable } from '@angular/core';
-import { FormControl } from '@angular/forms';
+import { FormControl, FormGroup } from '@angular/forms';
 
 @Injectable({
   providedIn: 'root'
@@ -15,5 +15,19 @@ export class ValidadoresService {
       }
     }
     return null;
+  }
+
+  passwords(pass1: string, pass2: string){
+    return (formGroup: FormGroup) => {
+      const paswword1 = formGroup.controls[pass1]
+      const paswword2 = formGroup.controls[pass2]
+      if(paswword1.value === paswword2.value){
+        paswword2.setErrors(null)
+      }else{
+        paswword2.setErrors({
+          noEsIgua: true
+        })
+      }
+    }
   }
 }
